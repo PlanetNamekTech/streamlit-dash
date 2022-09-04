1665487030653a3086b99b9743ff9a210dd3eaf4
PlanetNamekTech <sr23513@gmail.com>
2022-09-03 20:10:46 -0700

    troubleshooting image error

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,7 @@
+from urllib import response
 import streamlit as st
 import requests
+from requests.structures import CaseInsensitiveDict
 from decouple import config
 
 API_KEY = config('POLYGON_API_KEY')
@@ -7,10 +9,15 @@ symbol = st.sidebar.text_input("Symbol", value="MSFT")
 
 screen = st.sidebar.selectbox("View", ('Overview', 'Fundamentals', 'News', 'Ownership', 'Technicals'))
 
+print(API_KEY)
+
 st.title(screen)
 if screen == 'Overview':
   url = f"https://api.polygon.io/v3/reference/tickers/AAPL?apiKey={API_KEY}"
-  r = requests.get(url)
-  st.write(r.json())
+  headers = CaseInsensitiveDict()
+  headers["Authorization"] = "Bearer YPijiFRXWt61xNTCyGZOftwxq02SgZUG"
+  r = requests.get(url, headers=headers)
+  response_json = r.json()
+  st.image(response_json['results']['branding']['logo_url'])
 if screen == 'Fundamentals':
   pass
